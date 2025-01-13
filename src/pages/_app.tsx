db47458d8238bb1a6f35541a730cdd6e46842fb2
seungji2001 <lisa2009343123456@gmail.com>
2025-01-13 11:13:27 +0900

    section03: 프리패칭, api router

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,19 +2,25 @@ import "@/styles/globals.css"; //기본 스타일
 import type { AppProps } from "next/app";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 //Component -> page
 //pageProps -> 모든 Props 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
   const onClickButton = () =>{
-    router.push("/test");
+    router.push("/test"); //별도의 작업 해야함(onclick의 경우) link의 경우 자동으로 프리패칭 되지만 onclick은 안된다
   }
+
+  useEffect(()=>{
+    router.prefetch('/test') //마운트 이후, 프리패칭을 수행
+  },[])
+
   //pageProps를 모두 흩뿌려서 전달하고 있다
   return <>
     <header>
-      <Link href={"/"}>index</Link>
+      <Link href={"/"}>index</Link> 
       &nbsp;
-      <Link href={"/search"}>search</Link>
+      <Link href={"/search"} prefetch={false}>search</Link>
       &nbsp;
       <Link href={"/book/1"}>book</Link>
 
--- src/pages/book/[id].tsx
+++
@@ -1,8 +0,0 @@
-import {useRouter} from "next/router"
-
-//http://localhost:3000/book/13
-export default function Page(){
-    const router = useRouter();
-    const {id} = router.query;
-    return <h1>Book</h1>;
-}
