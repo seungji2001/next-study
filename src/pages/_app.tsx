f53238afd0129563836378517763de0ee25982d9
seungji2001 <lisa2009343123456@gmail.com>
2025-01-13 10:52:03 +0900

    section03:네비게이팅

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,27 @@
 import "@/styles/globals.css"; //기본 스타일
 import type { AppProps } from "next/app";
-
+import Link from "next/link";
+import { useRouter } from "next/router";
 //Component -> page
 //pageProps -> 모든 Props 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const onClickButton = () =>{
+    router.push("/test");
+  }
   //pageProps를 모두 흩뿌려서 전달하고 있다
   return <>
-    <header>글로벌 헤더</header>
+    <header>
+      <Link href={"/"}>index</Link>
+      &nbsp;
+      <Link href={"/search"}>search</Link>
+      &nbsp;
+      <Link href={"/book/1"}>book</Link>
+
+      <div>
+        <button onClick={onClickButton}>/test page 이동</button>
+      </div>
+    </header>
     <Component {...pageProps} />
   </>;
 }
