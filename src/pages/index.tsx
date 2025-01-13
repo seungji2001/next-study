99f20a1468c6b86c21c9bb225acb62dd7293a4a1
seungji2001 <lisa2009343123456@gmail.com>
2025-01-13 11:35:03 +0900

    section03: css가져오기

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,8 @@
+//css module을 사용하지 
+import style from "./index.module.css" //import css 안된다. //객체로 변환하여 호출하는 방향으로 한다
 export default function Home() { //default이다.
-  return <h1>인덱스</h1>;
+  return <>
+  <h1 className={style.h1}>인덱스</h1>
+  <h2 className={style.h2}>h2</h2>
+  </>
 }
